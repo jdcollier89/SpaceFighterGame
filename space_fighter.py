5ef42235ea02fa7f2b9a056eec7bcd9ae0929e24
JamieHome <j.d.collier@warwick.ac.uk>
2020-07-05 16:35:38 +0100

    Updated to include alien fleet - no movement yet.

--- space_fighter.py
+++ space_fighter.py
@@ -17,19 +17,17 @@ def run_game():
     ship = Ship(ai_settings, screen)
     # Make a group to store bullets in
     bullets = Group()
+    aliens = Group()
+
+    # Create the fleet of alients
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     # Start the main loop for the game
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        bullets.update()
-
-        # Get rid of bullets that have disappeared
-        for bullet in bullets.copy(): # Use copy so we don't modify the list we loop over
-            if bullet.rect.bottom <= 0:
-                bullets.remove(bullet)
-
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
         # Make the most recently drawn screen visible
         pygame.display.flip()
