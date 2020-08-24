930344a2c356dcb664e2136504bfad013a20ccd2
JamieHome <j.d.collier@warwick.ac.uk>
2020-08-24 23:52:21 +0100

    Added different banner image for pause and game over screens

--- splash_screen.py
+++ splash_screen.py
@@ -16,7 +16,10 @@ class SplashScreen:
         self.font_big = pygame.font.SysFont(None, 88)
         self.font_small = pygame.font.SysFont(None, 28)
 
+        self.open_flag = 1
+
         self.prep_banner()
+        self.prep_banner_mid()
         self.prep_open_screen()
 
     def prep_banner(self):
@@ -24,6 +27,12 @@ class SplashScreen:
         self.banner_rect = self.banner_img.get_rect()
         self.banner_rect.centerx = self.screen_rect.centerx
         self.banner_rect.centery = self.screen_rect.centery - 120
+
+    def prep_banner_mid(self):
+        self.banner_mid_img = pygame.image.load('images/banner_mid.png').convert_alpha()
+        self.banner_mid_rect = self.banner_mid_img.get_rect()
+        self.banner_mid_rect.centerx = self.screen_rect.centerx - 20
+        self.banner_mid_rect.centery = self.screen_rect.centery - 110
 
     def prep_instructions(self, instructions_str):
         """Generate message on how to start game"""
@@ -40,6 +49,7 @@ class SplashScreen:
         instructions_str = "Press P to play, or Q to quit"
         self.set_headline(open_screen_str)
         self.prep_instructions(instructions_str)
+        self.open_flag = 1
 
     def prep_game_over(self):
         """Set the game over message"""
@@ -47,13 +57,15 @@ class SplashScreen:
         instructions_str = "Press P to play, or Q to quit"
         self.set_headline(game_over_str)
         self.prep_instructions(instructions_str)
+        self.open_flag = 0
 
     def prep_pause_screen(self):
         """Set the paused message"""
         pause_screen_str = "GAME PAUSED"
-        instructions_str = "Press ESC key to resume play"
+        instructions_str = "Press ESC key to resume play, or Q to quit"
         self.set_headline(pause_screen_str)
         self.prep_instructions(instructions_str)
+        self.open_flag = 0
 
     def set_headline(self, input_str):
         """Set the contents and position of headline text"""
@@ -66,6 +78,10 @@ class SplashScreen:
 
     def draw_splash_screen(self):
         """Draw all splash screen contents"""
-        self.screen.blit(self.banner_img, self.banner_rect)
+        if self.open_flag == 1:
+            self.screen.blit(self.banner_img, self.banner_rect)
+        else:
+            self.screen.blit(self.banner_mid_img, self.banner_mid_rect)
+
         self.screen.blit(self.headline_image, self.headline_rect)
         self.screen.blit(self.insturctions_image, self.insturctions_rect)
